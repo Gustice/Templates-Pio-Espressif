efc8f6e9a9d905126391332362342cabdeb9851e
Gustice <jakob.ger@gmail.com>
2024-02-01 23:12:32 +0100

    feature: Create minimalistic c-example

--- src/main.c
+++ src/main.c
@@ -1,1 +1,19 @@
-void app_main() {}
+#include "esp_chip_info.h"
+#include "esp_flash.h"
+#include "esp_log.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "sdkconfig.h"
+#include <stdio.h>
+
+void app_main() {
+    vTaskDelay(pdMS_TO_TICKS(2000));
+    printf("Hello world!\n");
+
+    while (1) {
+        ESP_LOGI("main", "Tick");
+        vTaskDelay(pdMS_TO_TICKS(1000));
+        ESP_LOGW("main", "Tack");
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
+}
